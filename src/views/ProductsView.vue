4a2e94ed597dca8b581e67a4b97118fe1749a48d
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-12-01 09:40:28 -0300

    adicionando card de produtos

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -2,30 +2,20 @@
   <div>
     <h1 class="font-sans text-xl text-center mb-6">Lista de produtos</h1>
 
-    <div class="w-full pb-6 grid gap-4 grid-cols-3"> 
-      <div v-for="product in products" :key="product.id" class="relative mt-6 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-96">
-        <div class="p-6">
-          <h5 class="block mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
-            {{ product.name }}
-          </h5>
-          <p class="block font-sans text-base antialiased font-light leading-relaxed text-inherit">
-            {{ product.description }}
-          </p>
-          <div class="flex mt-3 justify-end">
-            <h5 class="text-2xl font-medium flex flex-1">{{ product.price }}</h5>
-            <button @click="addProduct(product)" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer">
-              <Icon icon="ic:outline-shopping-cart" />
-            </button>
-          </div>
-        </div>
-      </div> 
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 gap-4">
+      <ProductCard 
+        v-for="product in products" 
+        :key="product.id"
+        v-bind="product"
+        @btn_action="addProduct(product)"
+      />
     </div>
   </div>
 </template>
 
 <script setup>
 import { useCart } from '../composibles/useCart';
-import { Icon } from '@iconify/vue';
+import ProductCard from '../components/ProductCard/index.vue';
 
 const { products, addProduct } = useCart();
 </script>
