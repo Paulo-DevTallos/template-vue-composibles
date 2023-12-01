c2fd0fdc71605e827a39fc71b2ab13a16988c037
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-11-30 22:24:18 -0300

    implementando composibles

---
+++ src/composibles/useCart.js
@@ -0,0 +1,65 @@
+import { ref } from "vue";
+
+const products = ref([
+    {
+        id: 1,
+        name: 'Product 1',
+        description: 'Alguma descrição sobre o produto',
+        price: 'R$ 10,00'
+    },
+    {
+        id: 2,
+        name: 'Product 2',
+        description: 'Alguma descrição sobre o produto',
+        price: 'R$ 20,00'
+    },
+    {
+        id: 3,
+        name: 'Product 3',
+        description: 'Alguma descrição sobre o produto',
+        price: 'R$ 30,00'
+    },
+    {
+        id: 4,
+        name: 'Product 4',
+        description: 'Alguma descrição sobre o produto',
+        price: 'R$ 40,00'
+    }
+]);
+
+const cart = ref([]);
+const total = ref(0);
+
+export const useCart = () => {
+
+    function addProduct(product) {
+        const inCart = cart.value.some(item => item.id === product.id);
+        if (!inCart) cart.value.push(product);
+        return;
+    }
+
+    function removeProduct(id) {
+        const index = cart.value.findIndex(item => item.id === id);
+        cart.value.splice(index, 1);
+
+        //decrementar total
+        total.value -= Number(products.value.find(item => item.id === id).price
+            .replace('R$ ', '')
+            .replace(',', '.'));
+    }
+
+    function sumTotal() {
+        total.value = cart.value.reduce((acc, item) => {
+            return acc + Number(item.price.replace('R$ ', '').replace(',', '.'));
+        }, 0);
+    }
+
+    return {
+        products,
+        cart,
+        addProduct,
+        removeProduct,
+        total,
+        sumTotal,
+    }
+}
